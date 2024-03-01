6990ddfd6677b6de6ca97c9217e9ce440d783f55
mr-codr <github.nutlike917@passmail.net>
2024-03-01 08:52:35 -0300

    feat: better error handling

--- apps/mr-backend/src/infra/database.ts
+++ apps/mr-backend/src/infra/database.ts
@@ -8,11 +8,13 @@ const query = async (query: string | QueryObject) => {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  await client.connect();
+
   try {
+    await client.connect();
     const result = await client.query(query);
     return result;
   } catch (error) {
+    console.error(error);
     throw error;
   } finally {
     await client.end();
