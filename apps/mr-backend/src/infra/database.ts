7dadc167e5de774cc897754e2e02445064a35d6b
mr-codr <github.nutlike917@passmail.net>
2024-06-11 10:04:45 -0300

    feat: ssl

--- apps/mr-backend/src/infra/database.ts
+++ apps/mr-backend/src/infra/database.ts
@@ -7,6 +7,7 @@ const getNewClient = async () => {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === 'production',
   });
   client.connect();
   return client;
