86518a1a34235780b7e6fb90051384bc3872c738
mr-codr <github.nutlike917@passmail.net>
2024-07-08 09:38:40 -0300

    chore: stable tests

--- scripts/wait-for-postgres.js
+++ scripts/wait-for-postgres.js
@@ -3,16 +3,14 @@ const { exec } = require('node:child_process');
 console.log('🟡 waiting for postgres to accepts connections...');
 
 const checkPostgres = () => {
-  exec(
-    'docker exec postgres-dev pg_isready --host localhost',
-    (err, stdout) => {
-      if (stdout.search('accepting connections') === -1) {
-        checkPostgres();
-        return;
-      }
-      console.log('🟢 postgres ready to accepts connections!\n');
-    },
-  );
+  const command = 'docker exec postgres-dev pg_isready --host localhost';
+  exec(command, (err, stdout) => {
+    if (stdout.search('accepting connections') === -1) {
+      checkPostgres();
+      return;
+    }
+    console.log('🟢 postgres ready to accepts connections!\n');
+  });
 };
 
 checkPostgres();
